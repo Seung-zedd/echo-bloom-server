e16afdcd7101c88d94ea8b22ad6586c1ea68b0b2
seung-je <csz0209@gmail.com>
2025-08-02 15:31:18 +0900

    Initial commit

---
+++ src/main/java/com/checkmate/bub/BubApplication.java
@@ -0,0 +1,13 @@
+package com.checkmate.bub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubApplication.class, args);
+	}
+
+}
